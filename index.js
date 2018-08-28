e1fda0996db01dc34d71a7946e450bf60b57400f
DESKTOP-84IKM2Q\Desarrollo <ketrujillo08@gmail.com>
2018-08-28 16:05:04 -0500

    Push method allowed

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var credentials = { key: privateKey, cert: certificate };
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type,Accept');
-    res.header('Access-Control-Allow-Methods', 'PUT,GET,POST,DELETE,OPTIONS ');
+    res.header('Access-Control-Allow-Methods', 'PUT,PUSH,GET,POST,DELETE,OPTIONS');
     next();
 });
 
