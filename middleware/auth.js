7f20d4e1e2fdb31c603b686dc25464979eee6f12
DESKTOP-84IKM2Q\Desarrollo <ketrujillo08@gmail.com>
2018-10-23 18:50:43 -0500

    Validar Zapier Token

--- middleware/auth.js
+++ middleware/auth.js
@@ -22,6 +22,7 @@ getZapierToken = (req, res, next) => {
     if (zapToken != process.env.ZAPIER) {
         return res.status(500).json({
             exito: false,
+            zapToken,
             mensaje: "Token no válido"
         });
     } else {
